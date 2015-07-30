b3687533568accee714748d99c63fa995a2d06c4
Callum Kift <callumkift@gmail.com>
2015-07-30 19:29:29 +0200

    Method that converts from kelvin to degrees.

--- unitconverter.py
+++ unitconverter.py
@@ -5,6 +5,7 @@ Methods that convert weather data.
 
 import datetime as dt
 
+
 def datetimeconverter(uf_dt):
     """
     Takes a unix timestamp and returns datetime format
@@ -13,3 +14,14 @@ def datetimeconverter(uf_dt):
     """
 
     return dt.datetime.fromtimestamp(uf_dt)
+
+
+def temperatureconverter(temp_kelvin):
+    """
+    Converts temperature from kelvin to degrees
+    :param temp_kelvin: temperature in kelvin
+    :return: temperature in degrees
+    """
+
+    zerokelvin = 273.15
+    return temp_kelvin - zerokelvin
