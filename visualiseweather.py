98602c62f477547e123170ca6934972fd9670cc0
Callum Kift <callumkift@gmail.com>
2015-07-30 19:35:56 +0200

    In correct units.

--- visualiseweather.py
+++ visualiseweather.py
@@ -4,6 +4,7 @@ Methods that visualise the weather data.
 """
 
 import json
+import unitconverter as uc
 
 
 def printcurrent(current_json):
@@ -15,22 +16,22 @@ def printcurrent(current_json):
 
     weatherinfo = json.loads(current_json)
 
-    time = weatherinfo["dt"]
+    time = uc.datetimeconverter(weatherinfo["dt"]).strftime('%H:%M')
     wd = (weatherinfo["weather"][0])["description"]
-    temp = weatherinfo["main"]["temp"]
-    pressure = weatherinfo["main"]["pressure"]
-    humidity = weatherinfo["main"]["humidity"]
-    windspeed = weatherinfo["wind"]["speed"]
-    sunrise = weatherinfo["sys"]["sunrise"]
-    sunset = weatherinfo["sys"]["sunset"]
+    temp = uc.temperatureconverter(weatherinfo["main"]["temp"])  # degrees
+    pressure = weatherinfo["main"]["pressure"]  # hPa
+    humidity = weatherinfo["main"]["humidity"]  # %
+    windspeed = weatherinfo["wind"]["speed"]  # m / s
+    sunrise = uc.datetimeconverter(weatherinfo["sys"]["sunrise"]).strftime('%H:%M:%S')
+    sunset = uc.datetimeconverter(weatherinfo["sys"]["sunset"]).strftime('%H:%M:%S')
 
-    print time
-    print wd
-    print temp
-    print pressure
-    print humidity
-    print windspeed
-    print sunrise
-    print sunset
+    print "time: ", time
+    print "weather description: %s" % wd
+    print "temperature: %f" % temp
+    print "pressure: ", pressure
+    print "humidity: ", humidity
+    print "wind speed: %f" % windspeed
+    print "sunrise: ", sunrise
+    print "sunset: ", sunset
 
     return
