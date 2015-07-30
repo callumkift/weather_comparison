f342196287755492963c1b00a14411200a36939a
Callum Kift <callumkift@gmail.com>
2015-07-30 18:55:37 +0200

    Added method that extracts data from current weather json

--- visualiseweather.py
+++ visualiseweather.py
@@ -2,3 +2,35 @@
 """
 Methods that visualise the weather data.
 """
+
+import json
+
+
+def printcurrent(current_json):
+    """
+    Extracts data from current weather json
+    :param current_json: JSON of current weather
+    :return:
+    """
+
+    weatherinfo = json.loads(current_json)
+
+    time = weatherinfo["dt"]
+    wd = (weatherinfo["weather"][0])["description"]
+    temp = weatherinfo["main"]["temp"]
+    pressure = weatherinfo["main"]["pressure"]
+    humidity = weatherinfo["main"]["humidity"]
+    windspeed = weatherinfo["wind"]["speed"]
+    sunrise = weatherinfo["sys"]["sunrise"]
+    sunset = weatherinfo["sys"]["sunset"]
+
+    print time
+    print wd
+    print temp
+    print pressure
+    print humidity
+    print windspeed
+    print sunrise
+    print sunset
+
+    return
