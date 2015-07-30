462f833b9b4157b8b47c57d58af89a871ad8dd10
Callum Kift <callumkift@gmail.com>
2015-07-30 19:21:43 +0200

    Method added that converts unix time to datetime format.

--- unitconverter.py
+++ unitconverter.py
@@ -2,3 +2,14 @@
 """
 Methods that convert weather data.
 """
+
+import datetime as dt
+
+def datetimeconverter(uf_dt):
+    """
+    Takes a unix timestamp and returns datetime format
+    :param uf_dt: Unix timestamp
+    :return: datetime format
+    """
+
+    return dt.datetime.fromtimestamp(uf_dt)
