ed42e83299052c34f3647aab7bcb7ea8f89cbcd1
Callum Kift <callumkift@gmail.com>
2015-08-03 17:13:53 +0200

    Plots forecast and history overlay.

--- bokehvisualise.py
+++ bokehvisualise.py
@@ -5,13 +5,20 @@ Methods that create an interactive visualisation of the weather data.
 
 import json
 import unitconverter as uc
+import datetime as dt
 from collections import OrderedDict
 from bokeh.plotting import figure, show, output_file
 from bokeh.models import HoverTool, ColumnDataSource
 
 
 def interactiveplot(history_json, current_json, forecast_json):
-
+    """
+    Method that calls all other methods (controls) for the interactive plot.
+    :param history_json: JSON of weather history
+    :param current_json: JSON of current weather
+    :param forecast_json: JSON of weather forecast
+    :return:
+    """
     histdata = extracthistory(history_json)
     currdata = extractcurrent(current_json)
     foredata = extractforecast(forecast_json)
@@ -104,18 +111,40 @@ def extractforecast(forecast_json):
 
 
 def drawgraph(hist_list, curr_list, fore_list):
+    htime, htemp = zip(*hist_list)
 
-    t, temp = zip(*hist_list)
+    ctime = curr_list[0]; cwd = curr_list[1]; ctemp = curr_list[2]; cwspeed = curr_list[3]
+    csrise = curr_list[4]; csset = curr_list[5]; cpress = curr_list[6]; chum = curr_list[7]
+    ftime, fwd, ftemp, fwspeed, frain = zip(*fore_list)
+
+    htime, ftime = overlaptimes(htime, ftime)
 
     # output to static HTML file
-    output_file("hist.html", title="line plot example")
+    output_file("wc.html", title="Weather Comparison")
 
     # create a new plot with a title and axis labels
-    p = figure(title="simple line example", x_axis_label='x', y_axis_label='y')
+    p = figure(title="Weather Comparison", x_axis_label="Time", y_axis_label="Temperature")
 
     # add a line renderer with legend and line thickness
-    p.line(t, temp, line_width=2)
+    p.line(htime, htemp, color="blue", line_width=2, alpha=0.3)
+    p.line(ftime, ftemp, color="red", line_width=2)
 
     # show the results
     show(p)
     return
+
+
+def overlaptimes(htime, ftime):
+
+
+    oneday = 24
+    secsinhour = 60 * 60
+    fmintime = ftime[0]
+
+    hdates = [oneday + ((time - fmintime).total_seconds() / secsinhour) for time in htime]
+    fdates = [(time - fmintime).total_seconds() / secsinhour for time in ftime]
+
+    ht = [fmintime + dt.timedelta(hours=hour) for hour in hdates]
+    ft = [fmintime + dt.timedelta(hours=hour) for hour in fdates]
+
+    return ht, ft
